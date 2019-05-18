587e36e4f862135e931d995681157c49708dd004
ly <ly@example.com>
2019-05-18 11:38:15 +0800

    [+]

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,55 @@
 //index.js
-//获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    navText: [],
+    listData: []
   },
   onLoad: function () {
+    let navText = [
+      {
+        name:"待使用"+2,
+        isActive: true
+      },
+      {
+        name:"已赠送"+3,
+        isActive: false
+      },
+      {
+        name:"已使用"+4,
+        isActive: false
+      },
+      {
+        name:"已过期"+5,
+        isActive: false
+      }
+    ];
+    let listData = [
+      {
+        id: 1,
+        bgUrl: "/images/ticket-mine.png",
+        iconUrl: "/images/to_be_used.png",
+        name: "珠海公交专用",
+        money: "1.00",
+        source: "好友赠送"
+      }
+    ];
+    this.setData({
+      navText: navText,
+      listData: listData
+    });
   },
+  switch: function (e) {
+    console.log(e);
+    let index = e.currentTarget.dataset.id;
+    let navText = this.data.navText;
+    navText.forEach(element => {
+      element.isActive = false;
+    });
+    navText[index].isActive = true;
+    this.setData({
+      navText:navText
+    });
+  }
 })
